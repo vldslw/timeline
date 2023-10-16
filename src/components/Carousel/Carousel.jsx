ed94f20af6520698d7bc40382134443d872a825e
Vladislav Pavlov <vldslw@gmail.com>
2023-10-16 14:46:42 -0300

    feat: adjust css

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -22,7 +22,7 @@ export const Carousel = ({ onPeriodChange }) => {
   }, [tracker]);
 
   useLayoutEffect(() => {
-    items.current = gsap.utils.toArray(".item").reverse();
+    items.current = gsap.utils.toArray(".carousel__item").reverse();
     gsap.set(items.current, {
       motionPath: {
         path: "#circlePath",
@@ -33,7 +33,7 @@ export const Carousel = ({ onPeriodChange }) => {
     });
     tl.current = gsap
       .timeline({ paused: true })
-      .to(".wrapper", {
+      .to(".carousel__wrapper", {
         rotation: 360,
         transformOrigin: "center",
         duration: 1,
@@ -88,58 +88,70 @@ export const Carousel = ({ onPeriodChange }) => {
 
   return (
     <div className="carousel">
-      <div className="container">
-        <div className="wrapper">
+      <div className="carousel__container">
+        <div className="carousel__wrapper">
           <Path />
           <div
             id="item1"
             key={0}
-            className={`item ${tracker === 0 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 0 ? "carousel__item_active" : ""
+            }`}
           >
             1
           </div>
           <div
             id="item2"
             key={1}
-            className={`item ${tracker === 1 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 1 ? "carousel__item_active" : ""
+            }`}
           >
             2
           </div>
           <div
             id="item3"
             key={2}
-            className={`item ${tracker === 2 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 2 ? "carousel__item_active" : ""
+            }`}
           >
             3
           </div>
           <div
             id="item4"
             key={3}
-            className={`item ${tracker === 3 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 3 ? "carousel__item_active" : ""
+            }`}
           >
             4
           </div>
           <div
             id="item5"
             key={4}
-            className={`item ${tracker === 4 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 4 ? "carousel__item_active" : ""
+            }`}
           >
             5
           </div>
           <div
             id="item6"
             key={5}
-            className={`item ${tracker === 5 ? "item_active" : ""}`}
+            className={`carousel__item ${
+              tracker === 5 ? "carousel__item_active" : ""
+            }`}
           >
             6
           </div>
         </div>
       </div>
-      <div className="carousel-buttons">
-        <p className="carousel-paginator">{`${currentPage}/${totalPages}`}</p>
+      <div className="carousel__buttons">
+        <p className="carousel__paginator">{`${currentPage}/${totalPages}`}</p>
         <div
           id="prev"
-          className="carousel-button carousel-button_left"
+          className="carousel__button carousel__button_left"
           onClick={() => {
             trackerPrev();
             movePrev();
@@ -149,7 +161,7 @@ export const Carousel = ({ onPeriodChange }) => {
         </div>
         <div
           id="next"
-          className="carousel-button carousel-button_right"
+          className="carousel__button carousel__button_right"
           onClick={() => {
             trackerNext();
             moveNext();
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -3,9 +3,9 @@ import { ReactComponent as Bar } from "../../assets/bar.svg";
 
 export const Heading = () => {
   return (
-    <div className={styles.heading}>
-      <div className={styles.heading__bar}></div>
-      <h1 className={styles.heading__title}>Исторические даты</h1>
+    <div className={styles.header}>
+      <div className={styles.header__bar}></div>
+      <h1 className={styles.header__title}>Исторические даты</h1>
     </div>
   );
 };
